0785a4aed0d908d9325dca1bac7e4b2361229601
katelucas <lucas.katie@gmail.com>
2019-06-27 09:42:54 -0400

    basic unfinished book model

--- shelf/models.py
+++ shelf/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200, help_text="Book title ")
+    author = 
+    description = models.TextField(help_text="Brief description ", null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    # image = models.ImageField()
+
+    class Meta:
+        ordering = ['date_added']
+
+    # class Meta:
+    #     permissions = [
+    #         (),
+    #     ]
+    
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)]))
