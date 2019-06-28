9e78b91be9d2a0af0852632e263fdeedf9b9bb67
katelucas <lucas.katie@gmail.com>
2019-06-28 13:24:46 -0400

    basic homepage, Book model update

--- shelf/models.py
+++ shelf/models.py
@@ -3,14 +3,13 @@ from django.urls import reverse
 
 class Book(models.Model):
     title = models.CharField(max_length=200, help_text="Book title ")
-    # author = 
+    author = models.CharField(max_length=400, help_text="Author", null=True, blank=True)
     description = models.TextField(help_text="Brief description ", null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     # image = models.ImageField()
 
     class Meta:
         ordering = ['date_added']
-
     
     def __str__(self):
         return self.title
--- shelf/views.py
+++ shelf/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
+
+def index(request):
+    books = Book.objects.all()
+
+    context = {
+        'books': books
+    }
+    
+    return render(request, 'index.html', context=context)
 
 # Create your views here.
