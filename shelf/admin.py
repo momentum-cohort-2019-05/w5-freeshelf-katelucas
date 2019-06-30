63dff62df242161ea34da29835dcf4fcf3a96e43
katelucas <lucas.katie@gmail.com>
2019-06-30 18:31:17 -0400

    tryna migrate

--- shelf/admin.py
+++ shelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from shelf.models import Book
+from shelf.models import Category
 
 admin.site.register(Book)
 
--- shelf/models.py
+++ shelf/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.urls import reverse
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter the category for this book')
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
-    title = models.CharField(max_length=200, help_text="Book title ")
-    author = models.CharField(max_length=400, help_text="Author", null=True, blank=True)
-    description = models.TextField(help_text="Brief description ", null=True, blank=True)
+    title = models.CharField(max_length=200, help_text='Book title')
+    author = models.CharField(max_length=400, help_text='Author', null=True, blank=True)
+    description = models.TextField(help_text='Brief description', null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    url =  models.URLField(max_length=500, help_text='Book url', unique=True, null=True)
+    # category = ManyToManyField(category)
     # image = models.ImageField()
 
     class Meta:
@@ -14,5 +22,3 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse('book-detail', args=[str(self.id)]))
